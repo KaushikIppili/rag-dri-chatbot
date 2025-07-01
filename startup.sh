59f0a05995589a0ce75bdf5333a4d8c335458024
Kaushik Ippili <kippili@microsoft.com>
2025-07-02 01:04:57 +0530

    making changes to startup command

--- startup.sh
+++ startup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cd backend
+
+# Activate virtual environment if it exists
+if [ -d "antenv" ]; then
+  source antenv/bin/activate
+fi
+
+# Start the FastAPI app
 uvicorn main:app --host 0.0.0.0 --port 8000
