0b72651df58d48567dd9996a517dab01e2269d93
Kaushik Ippili <kippili@microsoft.com>
2025-07-02 13:02:11 +0530

    correcting openapi version for gpt4-o

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ TSG_INDEX = "tsgindex"
 openai.api_type = "azure"
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
 openai.api_key = os.getenv("AZURE_OPENAI_KEY")
-openai.api_version = "2024-02-01"
+openai.api_version = "2024-08-06"
 AZURE_DEPLOYMENT_NAME = os.getenv("AZURE_DEPLOYMENT_NAME")
 
 def search_index(query: str, index: str):
